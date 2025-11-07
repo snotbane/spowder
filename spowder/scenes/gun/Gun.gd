9c4b6c5222b840a4707d8d913952c187fe6de140
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-11-07 13:05:08 -0600

    Fixup

--- spowder/scenes/gun/Gun.gd
+++ spowder/scenes/gun/Gun.gd
@@ -1,11 +1,9 @@
 
 extends Node3D
 
-@onready var launcher : Node3D = $launcher
-
 @export var impulse_power := Vector2.ONE
 
 signal fired(global_impulse: Vector3)
 
 func fire() -> void:
-	fired.emit(-launcher.global_basis.z * Snotbane.expanded(impulse_power))
+	fired.emit(-global_basis.z * Snotbane.expanded(impulse_power))
