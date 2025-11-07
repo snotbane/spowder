e34d4a7cab0ac7581064f5d866b00c553d72233a
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-11-07 10:50:23 -0600

    Added basic turn input

---
+++ spowder/scenes/you/InputTurn.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export var affector_yaw : Node3D
+@export var affector_pitch : Node3D
+
+@export var turn_speed_mouse := Vector2.ONE
+@export var turn_speed_other := Vector2.ONE
+
+var _turn_input_mouse : Vector2
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED: return
+
+	if event is InputEventMouseMotion:
+		_turn_input_mouse = event.relative
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(delta: float) -> void:
+	var __turn_input_other := Input.get_vector(Snotbane.INPUT_GHOST_CAMERA_LEFT, Snotbane.INPUT_GHOST_CAMERA_RIGHT, Snotbane.INPUT_GHOST_CAMERA_DOWN, Snotbane.INPUT_GHOST_CAMERA_UP)
+
+	__turn_input_other = Vector2(__turn_input_other.y, -__turn_input_other.x) * turn_speed_other
+	_turn_input_mouse = Vector2(_turn_input_mouse.y, _turn_input_mouse.x) * turn_speed_mouse * -1.0
+
+	var __turn_vector := Snotbane.xy_(__turn_input_other + _turn_input_mouse)
+
+	affector_pitch.rotate_x(__turn_vector.x * delta)
+	affector_yaw.rotate_y(__turn_vector.y * delta)
+
+	affector_pitch.rotation_degrees.x = clampf(affector_pitch.rotation_degrees.x, -90.0, +90.0)
+
+	_turn_input_mouse = Vector2.ZERO
